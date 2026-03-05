a3f716a3f3431a26a6e2264658deba0d9718ce67
Megan <megan.young.digital@gmail.com>
2026-03-05 13:10:03 +0200

    Fixed portal route module export

---
+++ src/app/portal/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function PortalPage() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-black text-white">
+      Portal Working
+    </main>
+  );
+}
