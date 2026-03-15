b95be220759491397dc49dde99b8b3d05946f5d6
Megan <megan.young.digital@gmail.com>
2026-03-15 10:55:47 +0200

    Add Clerk sign-in and sign-up routes

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen">
       <SignUp />
     </div>
   );
